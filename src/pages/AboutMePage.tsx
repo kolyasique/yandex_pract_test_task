b7631f3d3933676a9aebd9d1cbcfc57abfcbfefc
Your Name <you@example.com>
2023-09-02 17:31:38 +0300

    done

--- src/pages/AboutMePage.tsx
+++ src/pages/AboutMePage.tsx
@@ -11,8 +11,8 @@ export default function AboutMePage({}: Props) {
     <>
         <Header />
             <PhotoModule/>
-            <VideoModule/>
             <MyHistoryModule/>
+            <VideoModule/>
         <Footer />
     </>
   )
